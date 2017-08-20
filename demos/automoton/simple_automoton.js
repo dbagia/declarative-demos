48c780f3e8a296ccd68a98120ab598e0ccb0721c
Dipen Bagia <dipen.bagia@gmail.com>
2017-08-20 18:36:46 +0530

    added simple-automoton solution

---
+++ demos/automoton/simple_automoton.js
@@ -0,0 +1,20 @@
+import R from 'ramda'
+
+export const stateRules = (state, command) => {
+
+	if(state === 'q1' && command === '0') return 'q1'
+
+	else if(state === 'q2' && command === '0') return 'q3'
+
+	else return 'q2'
+}
+
+export const readCommands = (commands, initialState) => {
+
+	const result = commands.reduce((acc, curr)=>{
+		return stateRules(acc, curr)
+	}, initialState)
+	
+	return R.equals(result, 'q2')
+
+}
