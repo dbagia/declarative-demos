bb0f0e95a17439172673c397c2a907c76135f639
Dipen Bagia <dipen.bagia@gmail.com>
2017-11-19 13:54:25 +0530

    Added standard and formatted code

--- demos/automoton/simple_automoton.js
+++ demos/automoton/simple_automoton.js
@@ -2,19 +2,19 @@ import R from 'ramda'
 
 export const stateRules = (state, command) => {
 
-	if(state === 'q1' && command === '0') return 'q1'
+  if (state === 'q1' && command === '0') return 'q1'
 
-	else if(state === 'q2' && command === '0') return 'q3'
+  else if (state === 'q2' && command === '0') return 'q3'
 
-	else return 'q2'
+  else return 'q2'
 }
 
 export const readCommands = (commands, initialState) => {
 
-	const result = commands.reduce((acc, curr)=>{
-		return stateRules(acc, curr)
-	}, initialState)
-	
-	return R.equals(result, 'q2')
+  const result = commands.reduce((acc, curr) => {
+    return stateRules(acc, curr)
+  }, initialState)
+
+  return R.equals(result, 'q2')
 
 }
