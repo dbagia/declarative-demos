7100a5fd5c39638c93a4387a5ae3ac2332079109
dipen bagia <dipen.bagia@klarna.com>
2019-12-14 22:48:36 +0100

    remove unused imports

--- demos/compare-triplets/compareTriplets.js
+++ demos/compare-triplets/compareTriplets.js
@@ -1,10 +1,3 @@
-import { Maybe } from 'crocks'
-import { isArray } from 'crocks/predicates'
-import { compose } from 'crocks/helpers'
-
-const areParamsArray = (a, b) => {
-    return isArray(a) && isArray(b)
-}
 // subtract :: Number -> Number -> Number
 const subtract = x => y =>
     x - y
@@ -12,6 +5,7 @@ const subtract = x => y =>
 const isNegative = i => i < 0
 const isPositive = i => i > 0
 
+// compareTriplets :: [Number] -> [Number] -> [Number]
 export function compareTriplets(a, b) {
     const partial = a.map(i => subtract(i))
     const subtracted = b.map(((j, index) => partial[index](j)))
