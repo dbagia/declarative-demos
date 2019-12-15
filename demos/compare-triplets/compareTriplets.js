084f40a288cddfb345d51362d61ec79a91f210f4
dipen bagia <dipen.bagia@klarna.com>
2019-12-15 19:06:20 +0100

    update naming

--- demos/compare-triplets/compareTriplets.js
+++ demos/compare-triplets/compareTriplets.js
@@ -1,14 +1,20 @@
+import { pipe, take, zipWith } from 'ramda'
 // subtract :: Number -> Number -> Number
-const subtract = x => y =>
+const subtract = (x, y) =>
     x - y
 
 const isNegative = i => i < 0
 const isPositive = i => i > 0
 
+// filterAndGetLength :: (a -> bool) -> [] -> Number
+const filterAndGetLength = (f, list) => list.filter(f).length
+
+const subtractBFromA = zipWith(subtract)
+const takeOnlyPositivesAndNegatives = x => x.filter(k => k !== 0)
+const compare = pipe(subtractBFromA, take(3), takeOnlyPositivesAndNegatives)
+
 // compareTriplets :: [Number] -> [Number] -> [Number]
 export function compareTriplets(a, b) {
-    const partial = a.map(i => subtract(i))
-    const subtracted = b.map(((j, index) => partial[index](j)))
-    const withoutZero = subtracted.filter(k => k !== 0)
-    return [withoutZero.filter(isPositive).length, withoutZero.filter(isNegative).length]
+    const comparison = compare(a, b)
+    return [filterAndGetLength(isPositive, comparison), filterAndGetLength(isNegative, comparison)]
 }
